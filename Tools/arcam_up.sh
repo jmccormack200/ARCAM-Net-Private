db7b2a7a2b46abee35533a5c45113f657075342d
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-09-19 19:05:45 -0400

    Debugging

--- Tools/arcam_up.sh
+++ Tools/arcam_up.sh
@@ -17,7 +17,7 @@ cd $workDir1 || cd $workDir2
 echo $2 | sudo -S ifconfig bat0 down
 echo $2 | sudo -S ifconfig tun0 down
 
-echo $2 | gksudo -S python ../Flowgraphs/broadcastwithFreqNoMac.py --tx-gain 45 --rx-gain 45 &
+echo $2 | sudo -S python ../Flowgraphs/broadcastwithFreqNoMac.py --tx-gain 45 --rx-gain 45 &
 
 sleep 10
 echo "Waiting for tun0 setup..."
@@ -27,7 +27,7 @@ sleep 10./
 
 echo $2 | sudo -S bash raiseBatSignal.sh
 
-last=$($(ifconfig eth0 || ifconfig eth1 || ifconfig eth2) | grep 'inet addr:' | cut -d: -f2 | awk -F'.' '{print $4}' | awk '{print $1}')
+last=$(ifconfig eth0 || ifconfig eth1 || ifconfig eth2 | grep 'inet addr:' | cut -d: -f2 | awk -F'.' '{print $4}' | awk '{print $1}')
 
 batip="192.168.200.$last"
 
